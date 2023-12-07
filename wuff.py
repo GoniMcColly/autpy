d8f5594bad5c50ed187a76e645cf7dfc7b25a8e5
Colin van Loo <colin@vanloo.ch>
2023-12-07 16:45:35 +0100

    feat: define commands using click
    ref #5

--- wuff.py
+++ wuff.py
@@ -3,7 +3,10 @@
 This script provides a command line interface to search for dogs, retrieve
 statistical information about those dogs, and to make up a new dog.
 """
+from datetime import datetime
 import csv
+import os
+import click
 import requests
 
 # pylint: disable=line-too-long
@@ -11,18 +14,52 @@ URL_DOG_DATA = "https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen_od100
 
 
 def get_dog_data(url):
-    """Gets data from zurich api"""
+    """Retrieve data from Zurich API."""
     r = requests.get(url, timeout=5)
     r.encoding = "utf-8-sig"
     return r.text.splitlines()
 
 
 def parse_csv(lines):
-    """Create csv.DictReader from data lines"""
+    """Create csv.DictReader from data lines."""
     return csv.DictReader(lines)
 
 
+@click.group()
+@click.option(
+    "--year", default=datetime.now().year, help="Limit output to specific year."
+)
+@click.pass_context
+def cli(ctx, year):
+    """Zürich Dog Tool"""
+    ctx.ensure_object(dict)
+    ctx.obj["year"] = year
+
+
+@cli.command()
+@click.pass_context
+@click.argument("name")
+def find(ctx, name):
+    """Find a dog by its name."""
+    click.echo(f"TODO: FIND {name}, {ctx.obj['year']}")
+
+
+@cli.command()
+@click.pass_context
+def stats(ctx):
+    """Print interesting stats about dog data."""
+    click.echo(f"TODO: STATS {ctx.obj['year']}")
+
+
+@cli.command()
+@click.option(
+    "--output-dir", "-o", default=os.getcwd(), help="Directory to save dog picture to."
+)
+@click.pass_context
+def create(ctx, output_dir):
+    """Make up a new dog at random."""
+    click.echo(f"TODO: CREATE {ctx.obj['year']}, {output_dir}")
+
+
 if __name__ == "__main__":
-    reader = parse_csv(get_dog_data(URL_DOG_DATA))
-    for row in reader:
-        print(row["HundenameText"])
+    cli(obj={})
