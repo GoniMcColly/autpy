20124c9cca66470e493cbece8d8345cfbf5a7bb8
leanx <agron.makolli@ost.ch>
2023-12-15 15:41:49 +0100

    feat: collect statistical data
    ref: #7

--- wuff.py
+++ wuff.py
@@ -92,7 +92,79 @@ def find(ctx, name):
 @click.pass_context
 def stats(ctx):
     """Print interesting stats about dog data."""
-    click.echo(f"TODO: STATS {ctx.obj['year']}")
+    # ***Pylint ist doof ;<<***
+    # pylint: disable=too-many-locals
+    reader = parse_csv(get_dog_data(URL_DOG_DATA))
+    longest_name = ""
+    shortest_name = None
+    male_name_count = {}
+    female_name_count = {}
+    male_dog_count = 0
+    female_dog_count = 0
+    first_year = None
+    last_year = 0
+
+    data = (
+        filter(lambda row: row["StichtagDatJahr"] == ctx.obj["year"], reader)
+        if ctx.obj["year"]
+        else reader
+    )
+    for row in data:
+        if row["HundenameText"] == "?":
+            continue
+        if len(row["HundenameText"]) > len(longest_name):
+            longest_name = row["HundenameText"]
+        if shortest_name is None or len(row["HundenameText"]) < len(shortest_name):
+            shortest_name = row["HundenameText"]
+        if first_year is None or first_year > int(row["StichtagDatJahr"]):
+            first_year = int(row["StichtagDatJahr"])
+        if last_year < int(row["StichtagDatJahr"]):
+            last_year = int(row["StichtagDatJahr"])
+        if sex_to_letter(row) == "m":
+            male_name_count[row["HundenameText"]] = male_name_count.get(
+                row["HundenameText"], 0
+            ) + int(row["AnzHunde"])
+            male_dog_count += int(row["AnzHunde"])
+        else:
+            female_name_count[row["HundenameText"]] = female_name_count.get(
+                row["HundenameText"], 0
+            ) + int(row["AnzHunde"])
+            female_dog_count += int(row["AnzHunde"])
+
+    top_male_name_sorted = sorted(
+        male_name_count.items(), key=lambda x: x[1], reverse=True
+    )[:10]
+    top_female_name_sorted = sorted(
+        female_name_count.items(), key=lambda x: x[1], reverse=True
+    )[:10]
+    top_overall_name_sorted = sorted(
+        top_male_name_sorted + top_female_name_sorted, key=lambda x: x[1], reverse=True
+    )[:10]
+    top_male_name_string = ", ".join(
+        [f"{name} ({count})" for name, count in top_male_name_sorted]
+    )
+    top_female_name_string = ", ".join(
+        [f"{name} ({count})" for name, count in top_female_name_sorted]
+    )
+    top_overall_name_string = ", ".join(
+        [f"{name} ({count})" for name, count in top_overall_name_sorted]
+    )
+
+    if first_year is None:
+        click.echo(f"No data available for year: {ctx.obj['year']}")
+        return
+    if ctx.obj["year"]:
+        click.echo(f"Showing stats for year: {ctx.obj['year']}")
+    else:
+        click.echo(f"Showing stats for years: {first_year} to {last_year}")
+    click.echo(f"The longest dog name is: {longest_name}")
+    click.echo(f"The shortest dog name is: {shortest_name}")
+    click.echo(f"Top ten most common names overall: {top_overall_name_string}")
+    click.echo(f"Top ten most common female names: {top_female_name_string}")
+    click.echo(f"Top ten most common male names: {top_male_name_string}")
+    click.echo(f"Total number of female dogs: {female_dog_count}")
+    click.echo(f"Total number of male dogs: {male_dog_count}")
+    click.echo(f"Total number of dogs: {female_dog_count+male_dog_count}")
 
 
 @cli.command()
